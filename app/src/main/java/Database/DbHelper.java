0a82d7f7b0ee935c285db41eb596284c620991b6
dvbreva <jump.the.stars@abv.bg>
2019-11-26 00:40:48 +0200

    Fix the sorting on main activity.

--- app/src/main/java/Database/DbHelper.java
+++ app/src/main/java/Database/DbHelper.java
@@ -22,7 +22,7 @@ public class DbHelper extends SQLiteOpenHelper {
     private static String categoryColumn = "category";
     private Context context;
 
-    public DbHelper(Context context){
+    public DbHelper(Context context) {
         super(context, dbName, null, 1);
     }
 
@@ -30,12 +30,12 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table " + tableName + "(" +
-                idColumn +  " integer primary key autoincrement, " +
+                idColumn + " integer primary key autoincrement, " +
                 nameColumn + " text, " +
                 phoneColumn + " text, " +
                 descriptionColumn + " text, " +
                 categoryColumn + " text " +
-                ")" );
+                ")");
     }
 
     @Override
@@ -44,46 +44,40 @@ public class DbHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    public List<Contact> GetAll(){
-        try
-        {
-            List<Contact> contactsList =  new ArrayList<Contact>();
+    public List<Contact> GetAll() {
+        try {
+            List<Contact> contactsList = new ArrayList<Contact>();
             SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-            Cursor cursor = sqLiteDatabase.rawQuery("select * from " + tableName,null);
+            Cursor cursor = sqLiteDatabase.rawQuery("select * from " + tableName + " order by " + nameColumn + " asc", null);
 
-            if(cursor.moveToFirst())
-            {
-                do
-                    {
-                        Contact contact = new Contact();
+            if (cursor.moveToFirst()) {
+                do {
+                    Contact contact = new Contact();
+                    contact.setId(cursor.getInt(0));
+                    contact.setName(cursor.getString(1));
+                    contact.setPhone(cursor.getString(2));
+                    contact.setDescription(cursor.getString(3));
+                    contact.setCategory(cursor.getString(4));
 
-                        contact.setId(cursor.getInt(0));
-                        contact.setName(cursor.getString(1));
-                        contact.setPhone(cursor.getString(2));
-                        contact.setDescription(cursor.getString(3));
-                        contact.setCategory(cursor.getString(4));
-                        contactsList.add(contact);
-                    }
-                    while(cursor.moveToNext());
+                    contactsList.add(contact);
+                }
+                while (cursor.moveToNext());
             }
 
             sqLiteDatabase.close();
             return contactsList;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return null;
         }
     }
 
-    public Contact GetById (int id){
-        try{
+    public Contact GetById(int id) {
+        try {
             SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-            Cursor cursor = sqLiteDatabase.rawQuery("select * from " + tableName + " where " + idColumn + " = ?" , new String[]{String.valueOf(id)});
+            Cursor cursor = sqLiteDatabase.rawQuery("select * from " + tableName + " where " + idColumn + " = ?", new String[]{String.valueOf(id)});
             Contact contact = null;
 
-            if (cursor.moveToFirst())
-            {
+            if (cursor.moveToFirst()) {
                 contact = new Contact();
                 contact.setId(cursor.getInt(0));
                 contact.setName(cursor.getString(1));
@@ -94,53 +88,13 @@ public class DbHelper extends SQLiteOpenHelper {
 
             sqLiteDatabase.close();
             return contact;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return null;
         }
     }
 
-    public boolean Create (Contact contact){
-        try
-        {
-            SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(nameColumn,contact.getName().toUpperCase());
-            contentValues.put(phoneColumn, contact.getPhone());
-            contentValues.put(descriptionColumn, contact.getDescription());
-            contentValues.put(categoryColumn,contact.getCategory());
-
-            long rows = sqLiteDatabase.insert(tableName,null,contentValues);
-
-            sqLiteDatabase.close();
-            return rows > 0;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
-
-    public boolean Delete (int id){
-        try
-        {
-            SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-            int rows = sqLiteDatabase.delete(tableName, idColumn + " =?" , new String[]{String.valueOf(id)});
-
-            sqLiteDatabase.close();
-            return rows > 0;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
-
-    public boolean Update(Contact contact){
-        try
-        {
+    public boolean Create(Contact contact) {
+        try {
             SQLiteDatabase sqLiteDatabase = getWritableDatabase();
 
             ContentValues contentValues = new ContentValues();
@@ -149,13 +103,42 @@ public class DbHelper extends SQLiteOpenHelper {
             contentValues.put(descriptionColumn, contact.getDescription());
             contentValues.put(categoryColumn, contact.getCategory());
 
-            int rows = sqLiteDatabase.update(tableName, contentValues, idColumn + " = ?", new String[] {String.valueOf(contact.getId())});
+            long rows = sqLiteDatabase.insert(tableName, null, contentValues);
 
             sqLiteDatabase.close();
             return rows > 0;
+        } catch (Exception e) {
+            return false;
         }
-        catch (Exception ex)
-        {
+    }
+
+    public boolean Delete(int id) {
+        try {
+            SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+            int rows = sqLiteDatabase.delete(tableName, idColumn + " =?", new String[]{String.valueOf(id)});
+
+            sqLiteDatabase.close();
+            return rows > 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean Update(Contact contact) {
+        try {
+            SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(nameColumn, contact.getName().toUpperCase());
+            contentValues.put(phoneColumn, contact.getPhone());
+            contentValues.put(descriptionColumn, contact.getDescription());
+            contentValues.put(categoryColumn, contact.getCategory());
+
+            int rows = sqLiteDatabase.update(tableName, contentValues, idColumn + " = ?", new String[]{String.valueOf(contact.getId())});
+
+            sqLiteDatabase.close();
+            return rows > 0;
+        } catch (Exception ex) {
             return false;
         }
     }
