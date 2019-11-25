1a786c1042b7c0e7f75763a012e28a87999cfb98
dvbreva <jump.the.stars@abv.bg>
2019-11-26 01:32:34 +0200

    Small changes.

--- app/src/main/java/com/example/contactsorganizer/ContactDetail.java
+++ app/src/main/java/com/example/contactsorganizer/ContactDetail.java
@@ -3,6 +3,7 @@ package com.example.contactsorganizer;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
